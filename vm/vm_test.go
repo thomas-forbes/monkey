c34cd81e7540a06f730a5ac125022a318020303a
thomas-forbes <34174315+thomas-forbes@users.noreply.github.com>
2026-03-19 12:49:10 +0000

    feat: add return value handling and function call execution

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -268,3 +268,62 @@ func TestIndexExpressions(t *testing.T) {
 	}
 	runVmTests(t, tests)
 }
+
+func TestCallingFunctionsWithoutArguments(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input:    "fn() { 5 + 10; }();",
+			expected: 15,
+		},
+		// {
+		// 	input:    `let one = fn() { 1; }; let two = fn() { 2; }; one() + two()`,
+		// 	expected: 3,
+		// },
+		// {
+		// 	input:    `let a = fn() { 1 }; let b = fn() { a() + 1 }; let c = fn() { b() + 1 }; c();`,
+		// 	expected: 3,
+		// },
+	}
+	runVmTests(t, tests)
+}
+
+func TestFunctionsWithReturnStatement(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input:    ` fn() { return 99; return 100; }(); `,
+			expected: 99,
+		},
+		// {
+		// 	input:    ` let earlyExit = fn() { return 99; return 100; }; earlyExit(); `,
+		// 	expected: 99,
+		// },
+	}
+	runVmTests(t, tests)
+}
+
+func TestFunctionsWithoutReturnValue(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input:    ` fn() { }(); `,
+			expected: object.NULL,
+		},
+		// {
+		// 	input:    ` let noReturn = fn() { }; let noReturnTwo = fn() { noReturn(); }; noReturn(); noReturnTwo(); `,
+		// 	expected: object.NULL,
+		// },
+	}
+	runVmTests(t, tests)
+}
+
+// func TestFirstClassFunctions(t *testing.T) {
+// 	tests := []vmTestCase{
+// 		{
+// 			input: `
+// 			let returnsOne = fn() { 1; };
+// 			let returnsOneReturner = fn() { returnsOne; };
+// 			returnsOneReturner()();`,
+// 			expected: 1,
+// 		},
+// 	}
+// 	runVmTests(t, tests)
+// }
